dd772ab8a92353532c8a61644b83cc21aa508bbf
nitr0n <nitr0n@gmx.de>
2018-02-09 01:05:54 +0100

    Added testproject

--- SortedCsv/README.md
+++ SortedCsv/README.md
@@ -7,3 +7,4 @@ Test Driven Development with PDD
 @todo #1:30min Create a line reader that is RFC Conform
 
 
+[![PDD status](http://www.0pdd.com/svg?name=yegor256/0pdd)](http://www.0pdd.com/p?name=yegor256/0pdd)
--- SortedCsvTests/CsvSorterTest.cs
+++ SortedCsvTests/CsvSorterTest.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using CsvSorter;
+using System.Text;
 
 namespace SortedCsvTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void SortedCsvInitialize()
         {
-
+            /* @todo #3 First there should only be a stream as Input to read the csv.
+            e.g. something like: 
+                CsvStream(StreamReader stream)
+            */
+            var sorted = new SortedCsvFile("SampleCSVFile_10600kb.csv", Encoding.UTF8, 0, '"', true, '"');
         }
     }
 }
